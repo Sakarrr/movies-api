94e30dd294062757fb562ed6d3fc0e45519b7c29
Sakarr <ctha.sakar@gmail.com>
2024-06-27 19:59:02 +0545

    Integrate - Mongo DB

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
+
+console.log(process.env.mongo_connect);
 
 app.get("/", (req, res) => {
   res.status(200).json({
