c92f0c6e1f8e4001317cbac2dc3fd3a5a89bd360
Sakarr <ctha.sakar@gmail.com>
2024-06-28 10:26:54 +0545

    Optimize - User data

--- handlers/createMovie.js
+++ handlers/createMovie.js
@@ -5,12 +5,13 @@ const createMovie = async (req, res) => {
   const MoviesModel = mongoose.model("movies"); // Should be exact to model name.
 
   console.log(req.body);
+  const { name, info, image, rating } = req.body;
 
   await MoviesModel.create({
-    name: req.body.name,
-    info: req.body.info,
-    image: req.body.image,
-    rating: req.body.rating,
+    name,
+    info,
+    image,
+    rating,
   });
 
   res.status(200).json({
