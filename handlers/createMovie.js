3be5e7b63143c6e54dd085ee612b397fec98ae9a
Sakarr <ctha.sakar@gmail.com>
2024-06-29 10:02:41 +0545

    Improvise - Validations via models

--- handlers/createMovie.js
+++ handlers/createMovie.js
@@ -7,28 +7,37 @@ const createMovie = async (req, res) => {
   const { name, info, image, rating } = req.body;
 
   // Validations
+  // try {
+  //   if (!name) throw "Name is required";
+
+  //   if (name.length < 3) throw "Movie name must be atleast 3 chars long";
+
+  //   if (!info) throw "Name is required";
+
+  //   if (rating > 10 || rating < 0) throw "Rating must be between 0-10";
+  // } catch (err) {
+  //   res.status(400).json({
+  //     status: "Failed",
+  //     message: err,
+  //   });
+  //   return;
+  // }
+
   try {
-    if (!name) throw "Name is required";
-
-    if (name.length < 3) throw "Movie name must be atleast 3 chars long";
-
-    if (!info) throw "Name is required";
-
-    if (rating > 10 || rating < 0) throw "Rating must be between 0-10";
-  } catch (err) {
+    await MoviesModel.create({
+      name,
+      info,
+      image,
+      rating,
+    });
+  } catch (e) {
+    console.log("Mongo Error");
     res.status(400).json({
       status: "Failed",
-      message: err,
+      message: e.message,
     });
     return;
   }
-
-  await MoviesModel.create({
-    name,
-    info,
-    image,
-    rating,
-  });
 
   res.status(200).json({
     messgae: "Movie was created",
--- models/movies.model.js
+++ models/movies.model.js
@@ -2,10 +2,23 @@ const mongoose = require("mongoose");
 
 // Create a model for movies
 const movieSchema = new mongoose.Schema({
-  name: { type: String },
-  info: { type: String },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name must be 3 chars long"],
+  },
+  info: {
+    type: String,
+    required: [true, "Info is required"],
+    minlength: [3, "Info must be 12 chars long"],
+  },
   image: { type: String },
-  rating: { type: Number },
+  rating: {
+    type: Number,
+    required: [true, "Rating is required"],
+    min: [0, "Must be between 0 - 10"],
+    max: [10, "Must be between 0 - 10"],
+  },
 });
 
 const movieModel = mongoose.model("movies", movieSchema);
