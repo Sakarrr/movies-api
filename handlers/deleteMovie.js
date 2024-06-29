3b45ed58603096992ad5a2691f1cc1ea9e4b84f1
Sakarr <ctha.sakar@gmail.com>
2024-06-29 10:55:06 +0545

    Add - Delete operation

--- handlers/deleteMovie.js
+++ handlers/deleteMovie.js
@@ -1,4 +1,23 @@
-const deleteMovie = (req, res) => {
+const mongoose = require("mongoose");
+
+const deleteMovie = async (req, res) => {
+  const MoviesModel = mongoose.model("movies");
+
+  const { _id } = req.query;
+
+  try {
+    if (!_id) throw "Please provide id to delete";
+  } catch (e) {
+    console.log("Mongo Error");
+    res.status(400).json({
+      status: "Failed",
+      message: e,
+    });
+    return;
+  }
+
+  await MoviesModel.deleteOne({ _id: _id });
+
   res.status(200).json({
     messgae: "Movie was deleted",
   });
